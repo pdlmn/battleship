ca17f2d7cc516998c07ed229a4ac8031bdff90eb
pdlmn <djeinwings@gmail.com>
2022-01-16 16:31:55 +0200

    Added event handler module

---
+++ src/dom/dom.js
@@ -0,0 +1,20 @@
+const playerBoard = document.querySelector('#player-board')
+const computerBoard = document.querySelector('#computer-board')
+
+const createCell = (isHidden) => {
+  const cell = document.createElement('div')
+  cell.classList.add('cell')
+  if (isHidden) cell.classList.add('fog-of-war')
+  return cell
+} 
+
+const renderBoard = (board, isHidden) => {
+  for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
+      board.append(createCell(isHidden))
+    }
+  }
+}
+
+renderBoard(playerBoard, false)
+renderBoard(computerBoard, true)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
+import './styles/style.css'
 import './factories/ship'
+import './dom/dom'
